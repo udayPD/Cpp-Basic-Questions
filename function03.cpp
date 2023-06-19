95146dc08092f8c7b9d064374c4496e955e5c27d
Uday Sankor Mohan <98579315+udayPD@users.noreply.github.com>
2023-06-19 19:27:38 +0530

    Add files via upload

---
+++ function03.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int maxx(int num1, int num2){
+    if(num1 > num2) return num1;
+    else return num2;
+}
+
+int main(){
+    int num1, num2 ;
+    cin >> num1 >> num2;
+    int minimum = maxx(num1, num2);
+    cout << minimum;
+}
