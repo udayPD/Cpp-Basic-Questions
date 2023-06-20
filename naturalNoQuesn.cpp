36205e443d24312f3ed5f8247bc0726d4aecfaf7
Uday Sankor Mohan <98579315+udayPD@users.noreply.github.com>
2023-06-20 21:15:57 +0530

    Add files via upload

---
+++ naturalNoQuesn.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+   int i;
+   int sum;
+   for(i=1;i<=10;i++){
+    cout << i << endl;
+
+    //Now to compute the sum of natural numbers 
+   sum = sum + i;
+   }
+   cout << "The sum is: " << sum;
+  
+}
