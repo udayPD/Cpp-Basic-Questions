cf916e3900fe7c9b8b4559913f6b91ff237b7107
Uday Sankor Mohan <98579315+udayPD@users.noreply.github.com>
2023-06-23 14:37:57 +0530

    Add files via upload

---
+++ Patterns/Question06.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+void print(int n){
+    for(int i=0;i<n;i++){
+       //Space
+       for(int j=0;j<n-i-1;j++){
+        cout << " ";
+       }
+       //STAR
+       for(int j =0;j<2*i+1;j++){
+        cout << "*";
+       }
+       //SPACE
+       for (int j=0;j<n-i-1;j++){
+        cout << " ";
+       }
+         cout << endl;
+
+    }
+   
+
+}
+
+
+
+
+int main(){
+     int n;
+     cin >> n;
+     print(n);
+}
