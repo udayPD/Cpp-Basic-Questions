83e1213484bc91724b72ab45efcad5a7b177b8a8
Uday Sankor Mohan <98579315+udayPD@users.noreply.github.com>
2023-06-25 21:00:19 +0530

    Add files via upload

---
+++ Basic/area.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float area,radius;
+    cout << "Enter the radius of the circle:" << endl;
+    cin >> radius;
+
+    area= 3.14*radius*radius;
+    cout << "The area of the circle is  " <<endl << area; 
+}
